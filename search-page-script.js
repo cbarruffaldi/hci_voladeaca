de7628ed1a62400c17a00a5a5e7514ad4418daa5
Luciano Bianchi <lbianchi@itba.edu.ar>
2016-10-05 19:25:03 -0300

    Cosas de filtros

---
+++ search-page-script.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function(){
+
+	$(".depart-time-button img").on("click", function(){
+		selectBorder(this);
+	});
+
+	var selectedTimeFilter = [];
+
+	function selectBorder(elem) {
+		var timebtn = $(elem)
+		var i = timebtn.data("dep-time");
+		console.log("CHANGE BORDER");
+		console.log(i);
+		
+		if (selectedTimeFilter[i]) {
+			selectedTimeFilter[i] = false;
+			timebtn.css("border-top", "");
+		} else {
+			selectedTimeFilter[i] = true;
+			timebtn.css("border-top", "5px solid black");
+		}
+	}
+});
+
+function updateValueText(value) {
+	$("#price-slider-value").text("$" + value);
+}
