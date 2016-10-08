c4fbcf58669dd754ac013be05bcf9c84b1a11011
marcelolynch <marcelomlynch@gmail.com>
2016-10-07 21:12:42 -0300

    "Todos los aeropuertos de una ciudad" en la lista

--- CheloLabs/acomplete.js
+++ CheloLabs/acomplete.js
@@ -1,31 +1,50 @@
 (function(){
 
-	ciudades = [];
+	airports = [];
     load();
 
     function load(){
       $.ajax({
-        url: "http://hci.it.itba.edu.ar/v1/api/geo.groovy?method=getcities&callback=listFill",
+        url: "http://hci.it.itba.edu.ar/v1/api/geo.groovy?method=getairports&callback=listFill",
         datatype: "jsonp"
       });
+
+      $.ajax({
+        url: "http://hci.it.itba.edu.ar/v1/api/geo.groovy?method=getcities&callback=cityFill",
+        datatype: "jsonp"
+      });
+
     }
 
     function fill(response){
+      //var r = [];
+      var ct = response.airports;
+
+      for(var i = 0; i < ct.length ; i++){
+        var nombre = response.airports[i].description;
+        var cityID = response.airports[i].id;
+        var disp = cityID + " - " + nombre;
+        airports.push({value: cityID, label: disp}); 
+      }
+    }
+
+        function cfill(response){
       //var r = [];
       var ct = response.cities;
 
       for(var i = 0; i < ct.length ; i++){
         var nombre = response.cities[i].name;
         var cityID = response.cities[i].id;
-        var disp = cityID + " - " + nombre;
-        ciudades.push({value: cityID, label: disp}); 
+        var disp = cityID + " - " + nombre + " (todos los aeropuertos)";
+        airports.push({value: cityID, label: disp}); 
       }
     }
 
-    window.listFill = fill;
 
+    window.listFill = fill;
+    window.cityFill = cfill;
 	new Awesomplete(document.getElementById("inputCity"), {
-	list: ciudades,
+	list: airports,
 	minChars: 1,
 	autoFirst: true}
 	);
