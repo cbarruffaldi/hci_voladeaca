82d743d28605e2da50edf88716fc01f9c899dcbf
luchob <lucianogbianchi@gmail.com>
2016-10-17 20:43:56 -0300

    Cambios psgbtn

--- psg-btn.js
+++ psg-btn.js
@@ -14,10 +14,26 @@ function updateNumbers() {
 	var t = passengers.total();
 	var str = t + " Pasajero" + ((t == 1) ? "" : "s");
 	$("#psg-dropdown .btn-content").text(str);
+
+	var btns = $(".passenger-row button");
+	//console.log(btns);
+
+	for (var i = 0; i < btns.length; i++) {
+		if ($(btns[i]).data("sum") == "minus") {
+			if  (passengers[$(btns[i]).data("info")] == 0) {
+				$(btns[i]).addClass("disabled");
+				$(btns[i]).attr("disabled", true);
+			}
+			else {
+				$(btns[i]).removeClass("disabled");
+				$(btns[i]).attr("disabled", false);
+			}
+		}
+	}
 }
 
-function zeroAdultsError() {
-	$("#zero-adults-msg").fadeIn();
+function zeroPaxError() {
+	$("#zero-psg-msg").fadeIn();
 }
 
 function updatePax(pressedbtn) {
@@ -26,19 +42,16 @@ function updatePax(pressedbtn) {
 		sum = -1;
 	else
 		sum = 1;
-	
-	var cat = $(pressedbtn).data("info");
-	
-	if (cat == "adults" && passengers[cat] + sum < 1)
-		zeroAdultsError();
-	else if(passengers[cat] + sum >= 0)
-		passengers[cat] += sum;
-		
-	updateNumbers();
-}
 
-function totalPassengers() {
-	return adults + children + infants;
+	var cat = $(pressedbtn).data("info");
+
+	passengers[cat] += sum;
+	if (passengers.total() == 0) {
+		passengers[cat] -= sum;
+		zeroPaxError();
+	}
+
+	updateNumbers();
 }
 
 jQuery(document).ready(function() {
@@ -52,14 +65,15 @@ jQuery(document).ready(function() {
 
 			if (okbtn.is(e.target) || (!psgdrop.is(e.target) && psgdrop.has(e.target).length === 0 && $(".open").has(e.target).length === 0)) {
 				psgdrop.parent().removeClass("open");
+				$(".passengers").find("#zero-psg-msg").fadeOut();
 			}
 		});
-	});	
+
+		updateNumbers();
+	});
 
 	$(".passenger-row button").on("click", function() {
-		$("#zero-adults-msg").fadeOut();
+		$("#zero-psg-msg").fadeOut();
 		updatePax(this);
 	});
-})
-
-window.passengers = passengers;
+});
