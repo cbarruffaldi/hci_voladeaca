c3be101dd6a2c12ee67dbfc9d6c9766e381b46a7
marcelolynch <marcelomlynch@gmail.com>
2016-10-19 00:42:35 -0300

    classic rookie mistake

--- js/searchbox.js
+++ js/searchbox.js
@@ -45,9 +45,9 @@ function initSearchbox(global){
 	jQuery(function($) {
 	 // CLEARABLE INPUT
  	function tog(v){return v?'addClass':'removeClass';} 
-  	$(document).on('input', '.clearable', function(){
+  	$(document)/*.on('input', '.clearable', function(){
     $(this)[tog(this.value)]('x');
-  	}).on('mousemove', '.x', function( e ){
+  	})*/.on('mousemove', '.x', function( e ){
     $(this)[tog(this.offsetWidth-18 < e.clientX-this.getBoundingClientRect().left)]('onX');   
  	 }).on('touchstart click', '.onX', function( ev ){
    	 ev.preventDefault();
