8f696474eefe9505db92f1293fe9b0e71ebaca0a
marcelolynch <marcelomlynch@gmail.com>
2016-10-20 01:47:53 -0300

    No andan las peticiones POST y quedo bastante feo, supongo, pero algo es algo

--- js/fix-reviewbox.js
+++
@@ -1,8 +0,0 @@
-	var elementPosition = $('#review-form').offset();
-	$(window).scroll(function(){
-        if($(window).scrollTop() > elementPosition.top -20){
-              $('#review-form').addClass("fixed-reviewform");
-        } else {
-            $('#review-form').removeClass("fixed-reviewform");
-        }    
-	});
