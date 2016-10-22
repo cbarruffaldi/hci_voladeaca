72cf31a409197437474e798ff65999a6e87a7ea3
Luciano Bianchi <lbianchi@itba.edu.ar>
2016-10-22 15:26:17 -0300

    Las distintas monedas nunca existieron

--- search-page-script.js
+++ search-page-script.js
@@ -20,81 +20,12 @@ jQuery(document).ready(function(){
 	});
 });
 
-
-
-//	$(function() {
-//
-//    var $sidebar   = $(".filter-area"), 
-//        $window    = $(window),
-//        offset     = $sidebar.offset(),
-//        topPadding = 15;
-//	
-//  	 var lastScroll = 0;
-//
-//
-//$(function() {
-//    var $sidebar   = $(".filter-area"), 
-//        $window    = $(window),
-//        offset     = $sidebar.offset(),
-//        topPadding = 15,
-//        bottomPadding = 350,
-//        windowHeight = $window.height();
-//
-//        var lastScroll = 0;
-//
-//    $window.scroll(function() {
-//    	var st = $(this).scrollTop();
-//
-//    	if(st < lastScroll){
-//        if ($window.scrollTop() < $sidebar.offset().top) {
-//            $sidebar.stop().animate({
-//                marginTop: $window.scrollTop() - offset.top + topPadding
-//            }, {duration: 0}, "_default");
-//        } else {
-//            $sidebar.stop().animate({
-//                marginBottom: 0
-//            }, {duration:50}, "_default");
-//
-//    	}
-//    	}
-//    	else if(st > lastScroll){
-//        if ($window.scrollTop() + $window.height() > $sidebar.position().top + $sidebar.outerHeight(true)) {
-//            $sidebar.stop().animate({
-//                marginTop: $window.scrollTop() - offset.top - bottomPadding
-//            }, {duration: 0}, "_default");
-//        } else {
-//            $sidebar.stop().animate({
-//                marginBottom: 0
-//            }, {duration:50}, "_default");
-//
-//    	}
-//
-//	}
-//    lastScroll = st;
-//
-//    });
-//    
-
-
-
-//});
-
-
-
-
-
-
 $("#order-dropdown a").on("click", function() {
 	var orderbtn = $("#orderby-btn")
 	orderbtn.text(this.text);
 	orderbtn.append('<span class="caret"></span>');
 });
 
-$("#currency-dropdown a").on("click", function() {
-	var currbtn = $("#currency-btn")
-	currbtn.text(this.text);
-	currbtn.append('<span class="caret"></span>');
-});
 
 $(".options-btn button").on("click", function(obj){
 	var toggleID = $(this).attr("data-toggle");
