97f319b7dc7c04714f4240c9c0273f064b79429c
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2016-11-13 22:40:26 -0300

    Iconito de buscar

--- Voladeacapp/app/src/main/java/hci/voladeacapp/FlightDetails.java
+++ Voladeacapp/app/src/main/java/hci/voladeacapp/FlightDetails.java
@@ -1,7 +1,13 @@
 package hci.voladeacapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class FlightDetails extends AppCompatActivity {
 
@@ -10,7 +16,38 @@ public class FlightDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight_details);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         String number = this.getIntent().getExtras().getString("number");
         setTitle(number);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.flight_details_menu, menu);
+
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search)
+                .getActionView();
+
+
+        searchView.setSearchableInfo(searchManager
+                .getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
 }
