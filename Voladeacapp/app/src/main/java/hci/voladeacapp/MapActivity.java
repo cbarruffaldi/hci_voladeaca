41416dde83f41b85b1d65a2bfa8779c253ed28a3
Chelo <mlynch@itba.edu.ar>
2016-11-14 14:29:57 -0300

    Mapa

---
+++ Voladeacapp/app/src/main/java/hci/voladeacapp/MapActivity.java
@@ -0,0 +1,225 @@
+package hci.voladeacapp;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.internal.PlaceOpeningHoursEntity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private TextView msg;
+    private EditText txt;
+    private RequestQueue requestQueue;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        txt = (EditText) findViewById(R.id.airportID);
+
+        Button button = (Button) findViewById(R.id.button);
+        msg = (TextView) findViewById(R.id.message);
+        if (button != null) {
+            button.setOnClickListener(new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new FillMapTask().execute("http://hci.it.itba.edu.ar/v1/api/booking.groovy?method=getflightdeals&from=" + txt.getText().toString());
+                }
+            });
+
+
+        }
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Maps Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+
+    private class FillMapTask extends AsyncTask<String, Void, JSONObject>{
+        @Override
+        protected JSONObject doInBackground(String... strings) {
+            // Instantiate the RequestQueue.
+            String url = strings[0];
+
+            // Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            JSONObject obj = null;
+                            try {
+                                obj = new JSONObject(response);
+                                fillMap(obj);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            fillMap(obj);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+            // Add the request to the RequestQueue;
+            requestQueue.add(stringRequest);
+
+            return null;
+
+        }
+
+        protected void fillMap(JSONObject obj){
+            mMap.clear();
+
+            try {
+                JSONArray arr = obj.getJSONArray("deals");
+
+
+                for(int i = 0; i < arr.length() ; i++){
+                    JSONObject deal = (JSONObject) arr.get(i);
+                    addNewMarker(deal);
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        private void addNewMarker(JSONObject deal) {
+            //{  "city":
+            //          {
+            //          "id":"USH",
+            //         "name":"Ushuaia, Tierra del Fuego",
+            //         "latitude":-54.7917,
+            //          "longitude":-68.2291,
+            //          "country":{"id":"AR","name":"Argentina"}
+            //          },
+            //
+            //  "price":737.80}
+            // }
+
+            msg.setText(deal.toString());
+            try {
+
+                JSONObject city = deal.getJSONObject("city");
+                msg.setText(city.toString());
+                double latitude = city.getDouble("latitude");
+                double longitude = city.getDouble("longitude");
+                String name = city.getString("name");
+
+                Double price = deal.getDouble("price");
+
+                name = name + " Precio: " + price.toString();
+
+                LatLng pos = new LatLng(latitude, longitude);
+                mMap.addMarker(new MarkerOptions().position(pos).title(name));
+
+            }catch(Exception e){
+                e.printStackTrace();
+                msg.setText(e.toString());
+            }
+        }
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
